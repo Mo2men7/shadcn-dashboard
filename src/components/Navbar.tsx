b606a5936e8004b51cdd2cf9ba8ab0a1c28c2d35
Momen Helmy <momen.helmy.m@gmail.com>
2025-08-21 17:46:38 +0300

    Using next theme to add night mode feature

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,6 @@
-import { Moon, User, Settings, LogOut } from "lucide-react";
+"use client";
+
+import { Moon, User, Settings, LogOut, Sun } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import {
@@ -10,8 +12,12 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
 
 function Navbar() {
+  const { setTheme } = useTheme();
+
   return (
     <nav className="p-4 flex items-center justify-between">
       <div>{/* Button to collapse */}</div>
@@ -19,9 +25,26 @@ function Navbar() {
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
 
-        <div>
-          <Moon />
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
 
         <DropdownMenu>
           <DropdownMenuTrigger>
