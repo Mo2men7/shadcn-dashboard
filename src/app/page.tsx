87da2c36f0fff25e0bb44523d0dcfdadb5b59b66
Momen Helmy <momen.helmy.m@gmail.com>
2025-08-22 17:35:34 +0300

    Using bar charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import AppBarChart from "@/components/AppBarChart";
+
 export default function Home() {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        Test grid
+        <AppBarChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test grid</div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test grid</div>
